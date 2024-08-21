79cd51493b1ba6f3c9a7c35ff9008585cebd23d9
Paul.Inglsmit <paul.inglsmit@gmail.com>
2024-08-21 12:43:32 +0530

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="w-4/5 m-auto bg-white h-screen rounded-xl shadow-xl mt-14">
+
+    <header>
+      <div>
+        <img src="/logo.png" alt="logo">
+        <div>
+          <h2>Sneakers Shop</h2>
+          <p>Movements is Life</p>
+        </div>
+      </div>
+    </header>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
