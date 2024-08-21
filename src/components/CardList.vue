7260a3c00f9fb8cb04c508419efc1b957525e132
Paul.Inglsmit <paul.inglsmit@gmail.com>
2024-08-21 18:14:49 +0530

    #02 - Card and CardList

---
+++ src/components/CardList.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Card from "@/components/Card.vue";
+
+</script>
+
+<template>
+  <div class="grid grid-cols-2 gap-5 xl:grid-cols-4">
+    <Card title="Reebok XNM" image-url="/sneakers/sneakers-1.jpg" :price="965" />
+    <Card title="Reebok XNM" image-url="/sneakers/sneakers-1.jpg" :price="965" :is-favorite="true" />
+    <Card title="Reebok XNM" image-url="/sneakers/sneakers-1.jpg" :price="965" is-added />
+    <Card title="Reebok XNM" image-url="/sneakers/sneakers-1.jpg" :price="965" />
+    <Card title="Reebok XNM" image-url="/sneakers/sneakers-1.jpg" :price="965" />
+    <Card title="Reebok XNM" image-url="/sneakers/sneakers-1.jpg" :price="965" />
+    <Card title="Reebok XNM" image-url="/sneakers/sneakers-1.jpg" :price="965" is-favorite />
+    <Card title="Reebok XNM" image-url="/sneakers/sneakers-1.jpg" :price="965" />
+    <Card title="Reebok XNM" image-url="/sneakers/sneakers-1.jpg" :price="965" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
