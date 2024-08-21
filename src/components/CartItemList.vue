e4c63c21146d05e8ade3aa037c25d35c8818e570
Paul.Inglsmit <paul.inglsmit@gmail.com>
2024-08-21 22:31:43 +0530

    #03 - Cart (Drawer)

---
+++ src/components/CartItemList.vue
@@ -0,0 +1,33 @@
+<script setup>
+import CartItem from "@/components/CartItem.vue";
+
+</script>
+
+<template>
+  <div class="flex flex-col gap-5">
+    <CartItem
+        title="Mans Sneakers Nike Blazer Mid Suede"
+        price="300"
+        img="/sneakers/sneakers-1.jpg"
+    />
+    <CartItem
+        title="Mans Sneakers Nike Blazer Mid Suede"
+        price="300"
+        img="/sneakers/sneakers-1.jpg"
+    />
+    <CartItem
+        title="Mans Sneakers Nike Blazer Mid Suede"
+        price="300"
+        img="/sneakers/sneakers-1.jpg"
+    />
+    <CartItem
+        title="Mans Sneakers Nike Blazer Mid Suede"
+        price="300"
+        img="/sneakers/sneakers-1.jpg"
+    />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Drawer.vue
@@ -0,0 +1,41 @@
+<script setup>
+import DrawerHead from "@/components/DrawerHead.vue";
+import CartItemList from "@/components/CartItemList.vue";
+</script>
+
+<template>
+  <div class="fixed z-10 top-0 h-full w-full bg-black opacity-70" />
+  <div
+      class="flex flex-col justify-between fixed z-10 top-0 h-full right-0 w-96 bg-white px-10 py-7"
+  >
+    <DrawerHead />
+    <div class="flex flex-col flex-1 justify-between">
+
+      <CartItemList />
+
+      <div>
+        <div class="flex flex-col gap-5">
+          <div class="flex items-end gap-2">
+            <span>Total:</span>
+            <div class="flex-1 border-b border-dashed" />
+            <span class="font-bold">$300</span>
+          </div>
+
+          <div class="flex items-end gap-2">
+            <span>Taxes 5%:</span>
+            <div class="flex-1 border-b border-dashed" />
+            <span class="font-bold">$15</span>
+          </div>
+        </div>
+
+        <button
+            disabled=""
+            class="flex justify-center items-center gap-3 w-full py-3 mt-10 bg-lime-500 text-white rounded-xl transition active:bg-lime-700 hover:bg-lime-600 disabled:bg-slate-300"
+        >
+          Checkout
+          <img src="/arrow-next.svg" alt="Arrow" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
