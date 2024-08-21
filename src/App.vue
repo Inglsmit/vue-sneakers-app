3c031afeb4d5a230ec297129e19325cf55230836
Paul.Inglsmit <paul.inglsmit@gmail.com>
2024-08-21 14:09:59 +0530

    #01 - Header component template

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <script setup>
 
+import Header from "@/components/Header.vue";
 </script>
 
 <template>
   <div class="w-4/5 m-auto bg-white h-screen rounded-xl shadow-xl mt-14">
 
-    <header>
-      <div>
-        <img src="/logo.png" alt="logo">
-        <div>
-          <h2>Sneakers Shop</h2>
-          <p>Movements is Life</p>
-        </div>
-      </div>
-    </header>
+    <Header/>
 
   </div>
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+    <div class="flex items-center gap-4">
+      <img src="/logo.png" alt="logo" class="w-10">
+      <div>
+        <h2 class="text-xl font-bold uppercase">Sneakers Shop</h2>
+        <p class="text-slate-500">Movements is Life</p>
+      </div>
+    </div>
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center gap-3 text-gray-500 cursor-pointer hover:text-black">
+        <img src="/cart.svg" alt="Cart">
+        <b>$200</b>
+      </li>
+
+      <li class="flex items-center gap-3 text-gray-500 cursor-pointer hover:text-black">
+        <img src="/heart.svg" alt="Favorites">
+        <span>Favorites</span>
+      </li>
+
+      <li class="flex items-center gap-3 text-gray-500 cursor-pointer hover:text-black">
+        <img src="/profile.svg" alt="Profile">
+        <span>Me</span>
+      </li>
+
+    </ul>
+  </header>
+
+</template>
+
+<style scoped>
+
+</style>
